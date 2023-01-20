74fb6f8cd7a148bc38f56e72e3f1ed888b943d02
alemcar <sandromuniz27@hotmail.com>
2023-01-20 10:30:02 -0400

    created angular project

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Course } from '../models/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent {
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Front-End',
+    },
+  ];
+
+  displayedColumns = ['name', 'category'];
+}
