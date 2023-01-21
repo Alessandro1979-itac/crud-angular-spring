6ebd8ae69cda0cace78f1e2d7e9de0b04981d382
alemcar <sandromuniz27@hotmail.com>
2023-01-21 18:56:25 -0400

    add project spring

---
+++ crud-spring/src/main/java/com/alemcar/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alemcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
