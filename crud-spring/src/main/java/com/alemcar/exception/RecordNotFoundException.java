c93bf060cbb12ca50286f2e4c60e32db53eb6b47
alemcar <sandromuniz27@hotmail.com>
2023-01-24 17:12:33 -0400

    add exceptions

--- crud-spring/src/main/java/com/alemcar/exception/RecordNotFoundException.java
+++ crud-spring/src/main/java/com/alemcar/exception/RecordNotFoundException.java
@@ -1,2 +1,8 @@
-package com.alemcar.exception;public class RecordNotFoundException {
+package com.alemcar.exception;
+
+public class RecordNotFoundException extends RuntimeException {
+
+    public RecordNotFoundException(Long id) {
+        super("Não foi possível encontrar o registro " + id);
+    }
 }
