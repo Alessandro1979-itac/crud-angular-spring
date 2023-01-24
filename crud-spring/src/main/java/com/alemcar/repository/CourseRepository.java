1af590f169fac7fddc69d12f4d6ef1e48d8f2c04
alemcar <sandromuniz27@hotmail.com>
2023-01-24 17:13:37 -0400

    add repository

--- crud-spring/src/main/java/com/alemcar/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/alemcar/repository/CourseRepository.java
@@ -1,2 +1,16 @@
-package com.alemcar.repository;public interface CourseRepository {
+package com.alemcar.repository;
+
+import com.alemcar.models.Course;
+import com.alemcar.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByStatus(Pageable pageable, Status status);
+
+    List<Course> findByName(String name);
 }
