123e042913b5865c9186946e0f436cc587c291c3
alemcar <sandromuniz27@hotmail.com>
2023-01-24 17:11:25 -0400

    add dto

--- crud-spring/src/main/java/com/alemcar/dto/CoursePageDTO.java
+++ crud-spring/src/main/java/com/alemcar/dto/CoursePageDTO.java
@@ -1,2 +1,9 @@
-package com.alemcar.dto;public record CoursePageDTO() {
+package com.alemcar.dto;
+
+import java.util.List;
+
+/**
+ * Used as response object that represents a Page with a list of Courses.
+ */
+public record CoursePageDTO(List<CourseDTO> courses, long totalElements, int totalPages) {
 }
--- crud-spring/src/main/java/com/alemcar/dto/CourseRequestDTO.java
+++ crud-spring/src/main/java/com/alemcar/dto/CourseRequestDTO.java
@@ -1,2 +1,17 @@
-package com.alemcar.dto;public record CourseRequestDTO() {
+package com.alemcar.dto;
+
+import com.alemcar.enums.Category;
+import com.alemcar.enums.constraints.ValueOfEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
+
+public record CourseRequestDTO(
+        @NotBlank @NotNull @Length(min = 5, max = 200) String name,
+        @NotBlank @NotNull @ValueOfEnum(enumClass = Category.class) String category,
+        @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
