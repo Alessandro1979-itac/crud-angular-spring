6ef763ccebc94423f95923252552f3915cfdb504
alemcar <sandromuniz27@hotmail.com>
2024-01-06 14:43:12 -0400

    add paginacao spring

--- crud-spring/src/main/java/com/alemcar/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/alemcar/CrudSpringApplication.java
@@ -24,23 +24,25 @@ public class CrudSpringApplication {
 		return args -> {
 			courseRepository.deleteAll();
 
-			Course c = new Course();
-			c.setName("Angular com Spring");
-			c.setCategory(Category.BACK_END);
-
-			Lesson l = new Lesson();
-			l.setName("Introdução");
-			l.setYoutubeUrl("watch?v=1");
-			l.setCourse(c);
-			c.getLessons().add(l);
-
-			Lesson l1 = new Lesson();
-			l1.setName("Angular");
-			l1.setYoutubeUrl("watch?v=2");
-			l1.setCourse(c);
-			c.getLessons().add(l1);
-
-			courseRepository.save(c);
+			for(int i = 0; i < 20; i++) {
+				Course c = new Course();
+				c.setName("Angular com Spring" + i);
+				c.setCategory(Category.BACK_END);
+	
+				Lesson l = new Lesson();
+				l.setName("Introdução");
+				l.setYoutubeUrl("watch?v=1");
+				l.setCourse(c);
+				c.getLessons().add(l);
+	
+				Lesson l1 = new Lesson();
+				l1.setName("Angular");
+				l1.setYoutubeUrl("watch?v=2");
+				l1.setCourse(c);
+				c.getLessons().add(l1);
+	
+				courseRepository.save(c);
+			}
 		};
 	}
 }
--- crud-spring/src/main/java/com/alemcar/service/CourseService.java
+++ crud-spring/src/main/java/com/alemcar/service/CourseService.java
@@ -3,18 +3,23 @@ package com.alemcar.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import com.alemcar.dto.CourseDTO;
+import com.alemcar.dto.CoursePageDTO;
 import com.alemcar.dto.mapper.CourseMapper;
 import com.alemcar.exception.RecordNotFoundException;
 import com.alemcar.model.Course;
 import com.alemcar.repository.CourseRepository;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @Service
 @Validated
@@ -28,11 +33,11 @@ public class CourseService {
         this.courseMapper = courseMapper;
     }
 
-    public List<CourseDTO> list() {
-        return courseRepository.findAll()
-                .stream()
-                .map(courseMapper::toDTO)
-                .collect(Collectors.toList());
+    public CoursePageDTO list(@PositiveOrZero int page, @Positive @Max(100) int pageSize) {
+        Page<Course> pageCourse = courseRepository.findAll(PageRequest.of(page, pageSize));
+        List<CourseDTO> courses = pageCourse.get().map(courseMapper::toDTO).collect(Collectors.toList());
+        
+        return new CoursePageDTO(courses, pageCourse.getTotalElements(), pageCourse.getTotalPages());
     }
 
     public CourseDTO findById(@NotNull @Positive Long id) {
